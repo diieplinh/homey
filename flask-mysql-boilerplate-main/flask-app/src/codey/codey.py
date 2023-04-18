b383e90bca7206146f1a06d6c576e9fdb023032d
Nour Tawfik <nourtawfik@Nours-MacBook-Air.local>
2023-04-17 20:41:22 -0400

    messages and events requests

--- flask-mysql-boilerplate-main/flask-app/src/codey/codey.py
+++ flask-mysql-boilerplate-main/flask-app/src/codey/codey.py
@@ -33,7 +33,7 @@ def get_user_info(user_id):
     return the_response
 
     # Gets  all recipients of messages from DB
-    @codey.route('/messages/<recipient_id>', methods['GET'])
+    @codey.route('/messages/<recipient_id>', methods=['GET'])
     def get_messages_info(recipient_id):
         cursor = db.get_db().cursor()
         cursor.execute('select sender_id, recipient_id, content, sent_at, message_id from Messages where reciptient_id = '%s', recipient_id)
@@ -46,9 +46,9 @@ def get_user_info(user_id):
     the_response.status_code = 200
     return the_response
 
-    # Gets the messages from the DB
+    # Posts the messages from the DB
     @codey.route('/messages', methods=['POST'])
-def get_users():
+def get_messages():
     cursor = db.get_db().cursor()
     cursor.execute('select sender_id, recipient_id, content, sent_at, message_id from Messages')
     row_headers = [x[0] for x in cursor.description]
@@ -59,3 +59,74 @@ def get_users():
     the_response = make_response(jsonify(json_data))
     the_response.status_code = 200
     return the_response
+
+# Gets the events from the DB
+@codey.route('/events', methods=['GET'])
+def get_events():
+    cursor = db.get_db().cursor()
+    cursor.execute('select title, details, scheduled, created_by, event_id from Events')
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    return the_response
+
+    # Gets the event_id from the DB
+    @codey.route('/events/<event_id>', methods=['GET'])
+def get_events_info(event_id):
+    cursor = db.get_db().cursor()
+    cursor.execute('select title, details, scheduled, created_by, event_id from Events where event_id = %s', event_id)
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    return the_response
+
+    # Posts the event from the DB
+    @codey.route('/events/<event_id>', methods=['POST'])
+def get_events_info(event_id):
+    cursor = db.get_db().cursor()
+    cursor.execute('select title, details, scheduled, created_by, event_id from Events where event_id = %s', event_id)
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    return the_response
+
+    # Put the event in the DB
+    @codey.route('/events/<event_id>', methods=['PUT'])
+def get_events_info(event_id):
+    cursor = db.get_db().cursor()
+    cursor.execute('select title, details, scheduled, created_by, event_id from Events where event_id = %s', event_id)
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    return the_response
+
+        # Delete the event from the DB
+    @codey.route('/events/<event_id>', methods=['DELETE'])
+def get_events_info(event_id):
+    cursor = db.get_db().cursor()
+    cursor.execute('select title, details, scheduled, created_by, event_id from Events where event_id = %s', event_id)
+    row_headers = [x[0] for x in cursor.description]
+    json_data = []
+    theData = cursor.fetchall()
+    for row in theData:
+        json_data.append(dict(zip(row_headers, row)))
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    return the_response
+
