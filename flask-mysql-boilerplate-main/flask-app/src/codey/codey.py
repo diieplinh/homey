c302284b29e7370a1ecf0ca36bdc11f5b7723910
jlin71321 <jlin71321@gmail.com>
2023-04-17 21:13:31 -0400

    formatting fixes and def with multiple http reqs allowed

--- flask-mysql-boilerplate-main/flask-app/src/codey/codey.py
+++ flask-mysql-boilerplate-main/flask-app/src/codey/codey.py
@@ -19,10 +19,28 @@ def get_users():
     return the_response
 
 # Gets information for the given userID from DB
-@codey.route('/users/<user_id>', methods=['GET'])
+@codey.route('/users/<user_id>', methods=['GET', 'DEL'])
 def get_user_info(user_id):
     cursor = db.get_db().cursor()
-    cursor.execute('select first_name, last_name, user_id from Users where user_id = %s', user_id)
+    json_data = []
+    if request.method == 'GET' :
+        cursor.execute('select first_name, last_name, user_id from Users where user_id = %s', user_id)
+        row_headers = [x[0] for x in cursor.description]
+        theData = cursor.fetchall()
+        for row in theData:
+            json_data.append(dict(zip(row_headers, row)))
+    elif request.method == 'DEL':
+        cursor.execute('delete from Users where user_id = %s', user_id)
+        db.get_db().commit()
+    the_response = make_response(jsonify(json_data))
+    the_response.status_code = 200
+    return the_response
+
+# Gets all recipients of messages from DB
+@codey.route('/messages/<recipient_id>', methods=['GET'])
+def get_messages_info(recipient_id):
+    cursor = db.get_db().cursor()
+    cursor.execute('select sender_id, recipient_id, content, sent_at, message_id from Messages where reciptient_id = %s', recipient_id)
     row_headers = [x[0] for x in cursor.description]
     json_data = []
     theData = cursor.fetchall()
@@ -32,22 +50,8 @@ def get_user_info(user_id):
     the_response.status_code = 200
     return the_response
 
-    # Gets  all recipients of messages from DB
-    @codey.route('/messages/<recipient_id>', methods=['GET'])
-    def get_messages_info(recipient_id):
-        cursor = db.get_db().cursor()
-        cursor.execute('select sender_id, recipient_id, content, sent_at, message_id from Messages where reciptient_id = '%s', recipient_id)
-    row_headers = [x[0] for x in cursor.description]
-    json_data = []
-    theData = cursor.fetchall()
-    for row in theData:
-        json_data.append(dict(zip(row_headers, row)))
-    the_response = make_response(jsonify(json_data))
-    the_response.status_code = 200
-    return the_response
-
-    # Posts the messages from the DB
-    @codey.route('/messages', methods=['POST'])
+# Posts the messages from the DB
+@codey.route('/messages', methods=['POST'])
 def get_messages():
     cursor = db.get_db().cursor()
     cursor.execute('select sender_id, recipient_id, content, sent_at, message_id from Messages')
@@ -74,8 +78,8 @@ def get_events():
     the_response.status_code = 200
     return the_response
 
-    # Gets the event_id from the DB
-    @codey.route('/events/<event_id>', methods=['GET'])
+# Gets the event_id from the DB
+@codey.route('/events/<event_id>', methods=['GET'])
 def get_events_info(event_id):
     cursor = db.get_db().cursor()
     cursor.execute('select title, details, scheduled, created_by, event_id from Events where event_id = %s', event_id)
@@ -88,8 +92,8 @@ def get_events_info(event_id):
     the_response.status_code = 200
     return the_response
 
-    # Posts the event from the DB
-    @codey.route('/events/<event_id>', methods=['POST'])
+# Posts the event from the DB
+@codey.route('/events/<event_id>', methods=['POST'])
 def get_events_info(event_id):
     cursor = db.get_db().cursor()
     cursor.execute('select title, details, scheduled, created_by, event_id from Events where event_id = %s', event_id)
@@ -102,8 +106,8 @@ def get_events_info(event_id):
     the_response.status_code = 200
     return the_response
 
-    # Put the event in the DB
-    @codey.route('/events/<event_id>', methods=['PUT'])
+# Put the event in the DB
+@codey.route('/events/<event_id>', methods=['PUT'])
 def get_events_info(event_id):
     cursor = db.get_db().cursor()
     cursor.execute('select title, details, scheduled, created_by, event_id from Events where event_id = %s', event_id)
@@ -116,8 +120,8 @@ def get_events_info(event_id):
     the_response.status_code = 200
     return the_response
 
-        # Delete the event from the DB
-    @codey.route('/events/<event_id>', methods=['DELETE'])
+# Delete the event from the DB
+@codey.route('/events/<event_id>', methods=['DELETE'])
 def get_events_info(event_id):
     cursor = db.get_db().cursor()
     cursor.execute('select title, details, scheduled, created_by, event_id from Events where event_id = %s', event_id)
